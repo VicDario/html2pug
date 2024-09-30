8b6487254acfcfee8f79ce508c27d1c2ae4788b5
VicDario <26585356+VicDario@users.noreply.github.com>
2024-09-30 16:49:58 -0300

    build: add ava eslint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 const globals = require('globals')
 const pluginJs = require('@eslint/js')
 const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended')
+const avaPlugin = require('eslint-plugin-ava')
 
 module.exports = [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
@@ -8,6 +9,9 @@ module.exports = [
   pluginJs.configs.recommended,
   eslintPluginPrettierRecommended,
   {
+    plugins: {
+      avaPlugin,
+    },
     rules: {
       'prettier/prettier': ['error', { singleQuote: true }],
     },
